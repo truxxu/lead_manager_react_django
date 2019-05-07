93954600a3496a626594985d3cbaf967d5274eb4
Daniel Trujillo <ganimedes@firemail.cc>
2019-05-07 12:17:29 +0200

    created react app

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   module: {
-    rules: {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
-    }
+    ]
   }
 }
